18f47dbb1869d3224bfc9828696e163d4c9e7610
Chaklader Asfak Arefe <chaklader@macs-MacBook-Pro.local>
2018-03-28 18:43:57 +0600

    stracture changed

---
+++ MT_Parallel_Udemy/ConcurrentLibrary/threadfactory/Task.java
@@ -0,0 +1,9 @@
+package com.baeldung.concurrent.threadfactory;
+
+public class Task implements Runnable {
+
+    @Override
+    public void run() {
+        // task details
+    }
+}
--- src/main/java/MessageQueue/README.txt
+++
@@ -1,2 +0,0 @@
-
-The tech test for the CANVA 
